7dc3d5eb0709a2557abc08c2306e8d8cc6c74e20
Guille <guillermo@artvisual.net>
2012-10-01 16:31:03 +0200

    .

--- Resources/ui/view.js
+++ Resources/ui/view.js
@@ -6,6 +6,41 @@ module.exports = function(subcategories) {
 		left:Ti.Platform.displayCaps.platformWidth - 1
 	});
 	
+	var tableView = Ti.UI.createTableView({
+		top:20,
+		left:20,
+		right:20,
+		bottom:20
+	});
+	
+	for	(i in subcategories) {
+		
+		var row = Ti.UI.createTableViewRow({
+			height:100
+		});
+		
+		var content = Ti.UI.createView();
+		
+		var title = Ti.UI.createLabel({
+			left:100
+		});
+		title.text = subcategories[i].name;
+		
+		var image = Ti.UI.createImageView({
+			left:20,
+			image:subcategories[i].image,
+			width:60,
+			height:'100%'
+		});
+		
+		content.add(title);
+		content.add(image);
+		
+		row.add(content);
+		
+		tableView.appendRow(row);
+		
+	}
 	
 	return view;
 	
