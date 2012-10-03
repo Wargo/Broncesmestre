ab879af6d575115610709aecd6b6ee3f9c1628df
Guille <guillermo@artvisual.net>
2012-10-03 09:00:30 +0200

    .

--- Resources/bbdd.js
+++ Resources/bbdd.js
@@ -7,8 +7,7 @@ module.exports = function() {
 				id:1,
 				name:'Herrajes decorativos',
 				num:603,
-				//image:'http://broncesmestre.com/catalogo/categorias/imagen/Catalogo/1.JPG',
-				image:'http://www.grb.es/content/files/descargaf_73_01_enter-plus-lavabo.jpg',
+				image:'/ui/images/cat1.jpg',
 				subcategories: [
 					{
 						id:1,
@@ -52,8 +51,7 @@ module.exports = function() {
 				id:2,
 				name:'Grifería artística',
 				num:643,
-				//image:'http://broncesmestre.com/catalogo/categorias/imagen/Catalogo/2.JPG'
-				image:'http://www.buscogremios.com/blog/wp-content/uploads/grifo.jpg',
+				image:'/ui/images/cat2.jpg',
 				subcategories: [
 					{
 						id:2,
@@ -88,8 +86,7 @@ module.exports = function() {
 				id:3,
 				name:'Iluminación decorativa',
 				num:8,
-				//image:'http://broncesmestre.com/catalogo/categorias/imagen/Catalogo/3.JPG'
-				image:'http://2.bp.blogspot.com/_WR1jCMemQvI/TTSOCH1vPEI/AAAAAAAAGQM/QSMWKlDUKnk/s1600/monomando%2Bblanco%2Bnegro%2Blavabo%2Bdise%25C3%25B1o%2Btres%2Bcuadro%2Bexclusive%2Bcascada.jpg',
+				image:'/ui/images/cat3.jpg',
 				subcategories: [
 					{
 						id:2,
